abddf6dbaa81c8b4507e54445c8366ff872d0e71
Jorrit Salverda <jsalverda@travix.com>
2021-06-08 11:45:17 +0200

    run snyk monitor before snyk test

--- clients/snykcli/client.go
+++ clients/snykcli/client.go
@@ -10,6 +10,7 @@ import (
 
 type Client interface {
 	Auth(ctx context.Context) (err error)
+	Monitor(ctx context.Context, flags api.SnykFlags) (err error)
 	Test(ctx context.Context, flags api.SnykFlags) (err error)
 }
 
@@ -27,6 +28,39 @@ type client struct {
 func (c *client) Auth(ctx context.Context) (err error) {
 	// snyk auth (https://support.snyk.io/hc/en-us/articles/360003812578-CLI-reference)
 	err = foundation.RunCommandExtended(ctx, "snyk auth %v", c.apiToken)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func (c *client) Monitor(ctx context.Context, flags api.SnykFlags) (err error) {
+	// snyk auth (https://support.snyk.io/hc/en-us/articles/360003812578-CLI-reference)
+	command := "snyk monitor"
+	if flags.FailOn != "" {
+		command += " --fail-on=" + flags.FailOn
+	}
+	if flags.File != "" {
+		command += " --file=" + flags.File
+	}
+	if flags.PackagesFolder != "" {
+		command += " --packages-folder=" + flags.PackagesFolder
+	}
+	if flags.SeverityThreshold != "" {
+		command += " --severity-threshold=" + flags.SeverityThreshold
+	}
+	if flags.AllProjects {
+		command += " --all-projects"
+	}
+	if len(flags.ExcludeDirectories) > 0 {
+		command += " --exclude=" + strings.Join(flags.ExcludeDirectories, ",")
+	}
+	if flags.Debug {
+		command += " -d"
+	}
+
+	err = foundation.RunCommandExtended(ctx, command)
 	if err != nil {
 		return
 	}
